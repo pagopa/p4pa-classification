f6819c8734727978ad3f167be105599d22fe3a4e
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2024-12-16 19:11:36 +0100

    init repo (#1)

--- src/main/java/it/gov/pagopa/pu/classification/ClassificationApplication.java
+++ src/main/java/it/gov/pagopa/pu/classification/ClassificationApplication.java
@@ -1,13 +1,13 @@
-package it.gov.pagopa.template;
+package it.gov.pagopa.pu.classification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateApplication {
+public class ClassificationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemplateApplication.class, args);
+		SpringApplication.run(ClassificationApplication.class, args);
 	}
 
 }
