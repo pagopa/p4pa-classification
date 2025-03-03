f9a77667b60c50f821bce21ed0e3ca0f4fbca0c1
MScarsella <marco.scarsella@emeal.nttdata.com>
2025-03-03 16:35:32 +0100

    feat: P4ADEV-2108-findPaymentsReportingDetailByFilters (#19)
    Co-authored-by: mscarsel <marco.scarsella@nttdata.com>

--- src/main/java/it/gov/pagopa/pu/classification/repository/PaymentsReportingRepository.java
+++ src/main/java/it/gov/pagopa/pu/classification/repository/PaymentsReportingRepository.java
@@ -1,11 +1,16 @@
 package it.gov.pagopa.pu.classification.repository;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import it.gov.pagopa.pu.classification.model.PaymentsReporting;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import java.util.List;
 
 
 @RepositoryRestResource(path = "payments-reporting")
@@ -20,4 +25,20 @@ public interface PaymentsReportingRepository extends JpaRepository<PaymentsRepor
     "p.transferIndex=:transferIndex")
   PaymentsReporting findBySemanticKey(Long organizationId, String iuv, String iur, int transferIndex);
 
+  @Query("""
+    SELECT p
+    FROM PaymentsReporting p
+    WHERE p.organizationId = :organizationId
+      AND p.iuf = :iuf
+      AND (:iuv IS NULL OR p.iuv = :iuv)
+      AND (cast(:payDateFrom AS DATE) IS NULL OR p.payDate >= :payDateFrom)
+      AND (cast(:payDateTo AS DATE) IS NULL OR p.payDate <= :payDateTo)
+    """)
+  Page<PaymentsReporting> findPaymentsReportingByFilters(
+    @Parameter(required = true, schema = @Schema(type = "integer", format = "int64")) @Param("organizationId") Long organizationId,
+    @Parameter(required = true) @Param("iuf") String iuf,
+    String iuv,
+    LocalDate payDateFrom,
+    LocalDate payDateTo,
+    Pageable pageable);
 }
