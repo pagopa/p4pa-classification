923a47e5a7d65539bb0bd1f3261ccf839a7eb2ba
antonio.torre <antonio.torre@pagopa.it>
2025-03-03 13:13:16 +0100

    fix: PII in REST_INVOKE logs

--- src/test/java/it/gov/pagopa/pu/classification/util/SecurityUtilsTest.java
+++ src/test/java/it/gov/pagopa/pu/classification/util/SecurityUtilsTest.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
+import java.net.URI;
+
 class SecurityUtilsTest {
 
   @AfterEach
@@ -46,6 +48,8 @@ class SecurityUtilsTest {
   }
 //endregion
 
+
+
   @Test
   void givenJwtWhenGetCurrentUserExternalIdThenReturnPrincipalName(){
     // Given
@@ -57,5 +61,16 @@ class SecurityUtilsTest {
 
     // Then
     Assertions.assertSame(principalName, result);
+  }
+
+  @Test
+  void givenUriWhenRemovePiiFromURIThenOk(){
+    String result = SecurityUtils.removePiiFromURI(URI.create("https://host/path?param1=PII&param2=noPII"));
+    Assertions.assertEquals("https://host/path?param1=***&param2=***", result);
+  }
+
+  @Test
+  void givenNullUriWhenRemovePiiFromURIThenOk(){
+    Assertions.assertNull(SecurityUtils.removePiiFromURI(null));
   }
 }
