95b8d1ecfba17895d904df7502bb271ad6d344d4
domenicogi <domenico.giacobbe@eng.it>
2025-03-05 17:37:59 +0100

    feat: P4ADEV-1998 crud for payment notification (#22)

---
+++ src/main/java/it/gov/pagopa/pu/classification/model/PaymentNotification.java
@@ -0,0 +1,70 @@
+package it.gov.pagopa.pu.classification.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.util.Date;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "payment_notification")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@EqualsAndHashCode(of = "paymentNotificationId", callSuper = false)
+public class PaymentNotification extends BaseEntity implements Serializable{
+
+  @Id
+  private String paymentNotificationId;
+  @NotNull
+  private Long organizationId;
+  @NotNull
+  private Long ingestionFlowFileId;
+  @NotNull
+  private String iud;
+  @NotNull
+  private String iuv;
+  @NotNull
+  private Date paymentExecutionDate;
+  @NotNull
+  private String paymentType;
+  @NotNull
+  private Long amountPaidCents;
+  private Long paCommission;
+  @NotNull
+  private String remittanceInformation;
+  @NotNull
+  private String transferCategory;
+  @NotNull
+  private String debtPositionTypeOrgCode;
+  private String balance;
+  private Long personalDataId;
+  private byte[] remittanceInformationHash;
+  private byte[] debtor_fiscal_code_hash;
+
+
+//region keep updated semanticId
+  public static String buildSemanticId(PaymentNotification paymentsReporting) {
+    return paymentsReporting.getIud() + "/" +
+      paymentsReporting.getOrganizationId();
+  }
+
+  private void setSemanticId() {
+    this.paymentNotificationId = buildSemanticId(this);
+  }
+
+  public void setOrganizationId(Long organizationId) {
+    this.organizationId = organizationId;
+    setSemanticId();
+  }
+
+  public void setIud(String iud) {
+    this.iud = iud;
+    setSemanticId();
+  }
+//endregion
+}
