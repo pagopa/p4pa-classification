709c91016bb7dd27d52dfe3eadf86b91e3d1bf2a
domenicogi <domenico.giacobbe@eng.it>
2025-03-12 10:29:51 +0100

    Update src/main/java/it/gov/pagopa/pu/classification/repository/AssessmentsDetailRepository.java
    Co-authored-by: antonioT90 <34568575+antonioT90@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/pu/classification/repository/AssessmentsDetailRepository.java
+++ src/main/java/it/gov/pagopa/pu/classification/repository/AssessmentsDetailRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
-@RepositoryRestResource(path = "assessments-detail")
+@RepositoryRestResource(path = "assessments-details")
 public interface AssessmentsDetailRepository extends JpaRepository<AssessmentsDetail,String> {
 
 }
