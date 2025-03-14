d71b5b7342d8decd6752596d1baeec745cc08624
ElisKina-dev <elis.kina@nttdata.com>
2025-03-14 14:07:53 +0100

    findByOrganizationIdAndTreasuryId

--- src/main/java/it/gov/pagopa/pu/classification/repository/TreasuryRepository.java
+++ src/main/java/it/gov/pagopa/pu/classification/repository/TreasuryRepository.java
@@ -1,16 +1,31 @@
 package it.gov.pagopa.pu.classification.repository;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import it.gov.pagopa.pu.classification.model.Treasury;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 
 @RepositoryRestResource(path = "treasury")
-public interface TreasuryRepository extends JpaRepository<Treasury,String> {
+public interface TreasuryRepository extends JpaRepository<Treasury, String> {
+
+  @Query("""
+    SELECT t
+    FROM Treasury t
+    WHERE t.organizationId = :organizationId
+    AND t.treasuryId = :treasuryId
+    """)
+  Optional<Treasury> findByOrganizationIdAndTreasuryId(
+    @Parameter(required = true, schema = @Schema(type = "integer", format = "int64")) @Param("organizationId") Long organizationId,
+    @Param("treasuryId") String treasuryId);
 
   @Transactional
   @Modifying
