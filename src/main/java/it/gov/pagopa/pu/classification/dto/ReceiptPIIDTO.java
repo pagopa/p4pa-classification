66729d12d29dbc886c91ca7da77629a7cbf279c1
macacia <marco.cacia@eng.it>
2025-04-01 16:16:00 +0200

    P4ADEV-2289 mappers

---
+++ src/main/java/it/gov/pagopa/pu/classification/dto/ReceiptPIIDTO.java
@@ -0,0 +1,16 @@
+package it.gov.pagopa.pu.classification.dto;
+
+import it.gov.pagopa.pu.debtposition.dto.generated.Person;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReceiptPIIDTO implements PIIDTO {
+  private Person debtor;
+  private Person payer;
+}
