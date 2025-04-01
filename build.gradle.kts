15bd6bcb563b63f23524f34cbbecb838a319c8a9
antonio.torre <antonio.torre@pagopa.it>
2025-04-01 17:06:06 +0200

    fix: reference to target branch during codeReview

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+import java.util.*
 
 plugins {
   java
@@ -195,7 +196,9 @@ openApiGenerate {
   ))
 }
 
-var targetEnv = when (grgit.branch.current().name) {
+println("TARGETBRANCH:::")
+println(System.getProperty("targetBranch"))
+var targetEnv = when (Objects.requireNonNullElse(System.getProperty("targetBranch"), grgit.branch.current().name)) {
   "uat" -> "uat"
   "main" -> "main"
   else -> "develop"
