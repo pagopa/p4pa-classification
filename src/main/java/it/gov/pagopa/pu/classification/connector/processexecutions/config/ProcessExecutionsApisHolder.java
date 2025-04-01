9b21dfce55c5cde7f645a91938c158b3331a6bdb
macacia <marco.cacia@eng.it>
2025-04-01 18:31:29 +0200

    Update ProcessExecutionsApisHolder.java

--- src/main/java/it/gov/pagopa/pu/classification/connector/processexecutions/config/ProcessExecutionsApisHolder.java
+++ src/main/java/it/gov/pagopa/pu/classification/connector/processexecutions/config/ProcessExecutionsApisHolder.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Lazy
 @Service
 public class ProcessExecutionsApisHolder {
 
