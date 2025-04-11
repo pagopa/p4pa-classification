89ab2cd349d75e8494d94b1fd0890fb9fe1dc34a
antonio.torre <antonio.torre@pagopa.it>
2025-04-11 18:37:03 +0200

    fix: Accepting LocalDateTime instead of OffsetDateTime in incoming JSON

---
+++ src/main/java/it/gov/pagopa/pu/classification/util/Constants.java
@@ -0,0 +1,12 @@
+package it.gov.pagopa.pu.classification.util;
+
+import java.time.ZoneId;
+
+public class Constants {
+
+  private Constants(){}
+
+  public static final ZoneId ZONEID = ZoneId.of("Europe/Rome");
+
+}
+
