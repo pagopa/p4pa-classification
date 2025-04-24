d7cf99463ff31f8abfd4d9a6c5417e0d9953ef04
ElisKina-dev <elis.kina@nttdata.com>
2025-04-24 16:31:55 +0200

    Classification detail API

--- src/main/java/it/gov/pagopa/pu/classification/repository/ClassificationRepository.java
+++ src/main/java/it/gov/pagopa/pu/classification/repository/ClassificationRepository.java
@@ -1,16 +1,21 @@
 package it.gov.pagopa.pu.classification.repository;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import it.gov.pagopa.pu.classification.enums.ClassificationsEnum;
 import it.gov.pagopa.pu.classification.model.Classification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 
 @RepositoryRestResource(path = "classifications")
-public interface ClassificationRepository extends JpaRepository<Classification,String> {
+public interface ClassificationRepository extends JpaRepository<Classification, String> {
   @Transactional
   @Modifying
   @RestResource(exported = false)
@@ -26,4 +31,7 @@ public interface ClassificationRepository extends JpaRepository<Classification,S
   @RestResource(exported = false)
   long deleteByOrganizationIdAndIuvAndIurAndTransferIndex(Long organizationId, String iuv, String iur, int transferIndex);
 
+  Optional<Classification> findByOrganizationIdAndClassificationId(
+    @Parameter(required = true, schema = @Schema(type = "integer", format = "int64")) @Param("organizationId") Long organizationId,
+    @Parameter(required = true, schema = @Schema(type = "integer", format = "int64")) @Param("classificationId") Long classificationId);
 }
