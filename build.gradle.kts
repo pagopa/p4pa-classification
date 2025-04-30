b502aac6d42d786cabbf020c0c3a348033e88e8f
macacia <marco.cacia@eng.it>
2025-04-30 15:57:37 +0200

    openapi

--- build.gradle.kts
+++ build.gradle.kts
@@ -187,7 +187,8 @@ openApiGenerate {
     "PaymentNotificationDTO" to "it.gov.pagopa.pu.classification.dto.PaymentNotificationDTO",
     "PersonDTO" to "it.gov.pagopa.pu.debtposition.dto.generated.PersonDTO",
     "FullClassificationView" to "it.gov.pagopa.pu.classification.dto.FullClassificationViewDTO",
-    "ClassificationsEnum" to "it.gov.pagopa.pu.classification.enums.ClassificationsEnum"
+    "ClassificationsEnum" to "it.gov.pagopa.pu.classification.enums.ClassificationsEnum",
+    "Balance" to "it.gov.pagopa.pu.classification.dto.assessments.BalanceDTO"
   ))
   configOptions.set(mapOf(
     "dateLibrary" to "java8",
