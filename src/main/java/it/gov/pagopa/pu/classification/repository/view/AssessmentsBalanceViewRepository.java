fb245faf7e0c1fd04aecf0a528a862e162a14a6a
macacia <marco.cacia@eng.it>
2025-05-05 16:40:15 +0200

    feat: P4ADEV-2761-assessment-API-for-SIL-WS (#73)

---
+++ src/main/java/it/gov/pagopa/pu/classification/repository/view/AssessmentsBalanceViewRepository.java
@@ -0,0 +1,41 @@
+package it.gov.pagopa.pu.classification.repository.view;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import it.gov.pagopa.pu.classification.model.view.AssessmentsBalanceView;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(path = "assessments-balance-view")
+public interface AssessmentsBalanceViewRepository extends Repository<AssessmentsBalanceView, String> {
+
+  @Query("""
+          SELECT new AssessmentsBalanceView(
+            ad.officeCode,
+            ad.debtPositionTypeOrgCode,
+            ad.sectionCode,
+            ad.assessmentCode,
+            SUM(ad.amountCents) AS amountCents
+          )
+          FROM AssessmentsDetail ad
+          JOIN Assessments a
+            ON ad.assessmentId = a.assessmentId
+          WHERE ad.organizationId = :organizationId
+            AND a.status = 'CLOSED'
+            AND ad.iud IN :iuds
+          GROUP BY  ad.officeCode,
+                    ad.debtPositionTypeOrgCode,
+                    ad.sectionCode,
+                    ad.assessmentCode
+          ORDER BY  ad.officeCode,
+                    ad.debtPositionTypeOrgCode,
+                    ad.sectionCode,
+                    ad.assessmentCode
+    """)
+  List<AssessmentsBalanceView> findClosedByOrganizationIdAndIuds(
+    @Parameter(required = true) @Param("organizationId") Long organizationId,
+    @Param("iuds") List<String> iuds);
+}
