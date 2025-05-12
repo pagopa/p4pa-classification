0b47268378231dc5537ae87f1e0e9a780040be0f
antonio.torre <antonio.torre@pagopa.it>
2025-05-12 13:18:50 +0200

    fix: paCommission renamed in paCommissionCents in full export view

--- src/main/java/it/gov/pagopa/pu/classification/mapper/FullClassificationViewPIIMapper.java
+++ src/main/java/it/gov/pagopa/pu/classification/mapper/FullClassificationViewPIIMapper.java
@@ -27,7 +27,7 @@ public class FullClassificationViewPIIMapper {
       .paymentNotificationPaymentExecutionDate(noPii.getPaymentNotificationPaymentExecutionDate())
       .paymentNotificationPaymentType(noPii.getPaymentNotificationPaymentType())
       .paymentNotificationAmountPaidCents(noPii.getPaymentNotificationAmountPaidCents())
-      .paymentNotificationPaCommission(noPii.getPaymentNotificationPaCommissionCents())
+      .paymentNotificationPaCommissionCents(noPii.getPaymentNotificationPaCommissionCents())
       .paymentNotificationRemittanceInformation(noPii.getPaymentNotificationRemittanceInformation())
       .paymentNotificationTransferCategory(noPii.getPaymentNotificationTransferCategory())
       .paymentNotificationDebtPositionTypeOrgCode(noPii.getPaymentNotificationDebtPositionTypeOrgCode())
