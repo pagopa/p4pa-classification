d5386b6243105a10b42cd5e1e9226be28846e17b
RiccardoGiuliani <riccardo.giuliani@nttdata.com>
2025-06-10 16:38:24 +0200

    P4ADEV-3136 find assessments registry by filters

--- src/main/java/it/gov/pagopa/pu/classification/repository/AssessmentsRegistryRepository.java
+++ src/main/java/it/gov/pagopa/pu/classification/repository/AssessmentsRegistryRepository.java
@@ -2,10 +2,19 @@ package it.gov.pagopa.pu.classification.repository;
 
 import it.gov.pagopa.pu.classification.model.AssessmentsRegistry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 
 @RepositoryRestResource(path = "assessments-registries")
 public interface AssessmentsRegistryRepository extends JpaRepository<AssessmentsRegistry,Long> {
 
+  @Query("SELECT a FROM AssessmentsRegistry a WHERE "
+    + "(:debtPositionTypeOrgCode IS NULL OR a.debtPositionTypeOrgCode = :debtPositionTypeOrgCode) AND "
+    + "(:organizationId IS NULL OR a.organizationId = :organizationId) AND "
+    + "(:operatingYear IS NULL OR a.operatingYear = :operatingYear) AND "
+    + "(:status IS NULL OR a.status = :status)")
+  AssessmentsRegistry findAssessmentsRegistryByFilters(String debtPositionTypeOrgCode,
+    Long organizationId,
+    String operatingYear,
+    String status);
 }
