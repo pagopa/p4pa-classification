37ace5105fc06e0c13f2f23e240fd6281e837a1b
macacia <marco.cacia@eng.it>
2025-08-06 09:55:52 +0200

    new repository apis addition

--- src/main/java/it/gov/pagopa/pu/classification/repository/view/AssessmentsBalanceViewRepository.java
+++ src/main/java/it/gov/pagopa/pu/classification/repository/view/AssessmentsBalanceViewRepository.java
@@ -38,4 +38,68 @@ public interface AssessmentsBalanceViewRepository extends Repository<Assessments
   List<AssessmentsBalanceView> findClosedByOrganizationIdAndIuds(
     @Parameter(required = true) @Param("organizationId") Long organizationId,
     @Param("iuds") List<String> iuds);
+
+  @Query("""
+    SELECT new AssessmentsBalanceView(
+            ad.officeCode,
+            ad.debtPositionTypeOrgCode,
+            ad.sectionCode,
+            ad.assessmentCode,
+            SUM(ad.amountCents) AS amountCents
+          )
+          FROM  AssessmentsDetail ad
+          JOIN  Assessments a
+            ON  ad.assessmentId = a.assessmentId
+          JOIN  Classification c
+            ON  ad.iud = c.iud
+            AND ad.organizationId = c.organizationId
+          WHERE c.organizationId = :organizationId
+            AND a.status = 'CLOSED'
+            AND c.iuf = :iuf
+            AND c.label = 'RT_IUF'
+          GROUP BY  ad.officeCode,
+                    ad.debtPositionTypeOrgCode,
+                    ad.sectionCode,
+                    ad.assessmentCode
+          ORDER BY  ad.officeCode,
+                    ad.debtPositionTypeOrgCode,
+                    ad.sectionCode,
+                    ad.assessmentCode
+    """)
+  List<AssessmentsBalanceView> findClosedByOrganizationIdAndIuf(
+  @Parameter(required = true) @Param("organizationId") Long organizationId,
+  @Parameter(required = true) @Param("iuf") String iuf);
+
+  @Query("""
+    SELECT new AssessmentsBalanceView(
+            ad.officeCode,
+            ad.debtPositionTypeOrgCode,
+            ad.sectionCode,
+            ad.assessmentCode,
+            SUM(ad.amountCents) AS amountCents
+          )
+          FROM  AssessmentsDetail ad
+          JOIN  Assessments a
+            ON  ad.assessmentId = a.assessmentId
+          JOIN  Classification c
+            ON  ad.iud = c.iud
+            AND ad.organizationId = c.organizationId
+          WHERE c.organizationId = :organizationId
+            AND a.status = 'CLOSED'
+            AND c.billCode = :billCode
+            AND c.billYear = :billYear
+            AND c.label = 'RT_IUF_TES'
+          GROUP BY  ad.officeCode,
+                    ad.debtPositionTypeOrgCode,
+                    ad.sectionCode,
+                    ad.assessmentCode
+          ORDER BY  ad.officeCode,
+                    ad.debtPositionTypeOrgCode,
+                    ad.sectionCode,
+                    ad.assessmentCode
+    """)
+  List<AssessmentsBalanceView> findClosedByOrganizationIdAndBill(
+    @Parameter(required = true) @Param("organizationId") Long organizationId,
+    @Parameter(required = true) @Param("billCode") String billCode,
+    @Parameter(required = true) @Param("billYear") String billYear);
 }
