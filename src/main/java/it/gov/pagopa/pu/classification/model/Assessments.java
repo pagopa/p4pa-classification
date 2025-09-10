95e290a22ee1205cf260377a66f9da208f326cbc
antonio.torre <antonio.torre@pagopa.it>
2025-09-10 15:29:02 +0200

    chore: Updating CODEOWNERS

--- src/main/java/it/gov/pagopa/pu/classification/model/Assessments.java
+++ src/main/java/it/gov/pagopa/pu/classification/model/Assessments.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 @Builder
 @Data
 @EqualsAndHashCode(callSuper = false)
+@DataEventTraced(evenType = ....)
 public class Assessments extends BaseEntity implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "assessments_generator")
