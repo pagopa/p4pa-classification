74aeba8d587ed3ab6a348c174357502baa9b3acd
antoniocalo <antonio.calo@nttdata.com>
2025-10-01 11:15:51 +0200

    P4ADEV-3902-change-assessments-event

--- src/test/java/it/gov/pagopa/pu/classification/event/producer/DataEventsProducerServiceTest.java
+++ src/test/java/it/gov/pagopa/pu/classification/event/producer/DataEventsProducerServiceTest.java
@@ -3,10 +3,12 @@ package it.gov.pagopa.pu.classification.event.producer;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import it.gov.pagopa.pu.classification.dto.AssessmentsDataDTO;
+import it.gov.pagopa.pu.classification.dto.AssessmentsDetailDataDTO;
 import it.gov.pagopa.pu.classification.enums.DataEventType;
 import it.gov.pagopa.pu.classification.event.dto.DataEventDTO;
 import it.gov.pagopa.pu.classification.event.dto.DataEventRequestDTO;
+import it.gov.pagopa.pu.classification.model.Assessments;
+import it.gov.pagopa.pu.classification.model.AssessmentsDetail;
 import it.gov.pagopa.pu.classification.model.Classification;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
@@ -43,16 +45,20 @@ class DataEventsProducerServiceTest {
   @Test
   void whenNotifyAssessmentsEventThenSendMessage() {
     // Given
-    AssessmentsDataDTO assessmentsDataDTO = new AssessmentsDataDTO();
-    assessmentsDataDTO.setAssessmentId(1L);
-    assessmentsDataDTO.setAssessmentDetailId(99L);
+    AssessmentsDetailDataDTO assessmentsDataDTO = new AssessmentsDetailDataDTO();
+    Assessments assessments = new Assessments();
+    assessments.setAssessmentId(1L);
+    AssessmentsDetail assessmentsDetail = new AssessmentsDetail();
+    assessmentsDetail.setAssessmentDetailId(99L);
+    assessmentsDataDTO.setAssessments(assessments);
+    assessmentsDataDTO.setAssessmentsDetailList(List.of(assessmentsDetail));
 
-    DataEventRequestDTO dataEventRequestDTO = new DataEventRequestDTO(DataEventType.ASSESSMENTS, "EVENTDESCRIPTION");
+    DataEventRequestDTO dataEventRequestDTO = new DataEventRequestDTO(DataEventType.ASSESSMENTS_DETAIL, "EVENTDESCRIPTION");
     String traceId = "TRACEID";
     MDC.put("traceId", traceId);
 
     // When
-    dataEventsProducerService.notifyAssessmentsEvent(assessmentsDataDTO, dataEventRequestDTO);
+    dataEventsProducerService.notifyAssessmentsDetailEvent(assessmentsDataDTO, dataEventRequestDTO);
 
     // Then
     verify(streamBridge, times(1)).send(
@@ -60,12 +66,12 @@ class DataEventsProducerServiceTest {
       Mockito.any(),
       Mockito.<Message<?>>argThat(m -> {
         DataEventDTO<?> payload = (DataEventDTO<?>)m.getPayload();
-        String eventIdPrefix = dataEventRequestDTO.getDataEventType().name() + assessmentsDataDTO.getAssessmentId();
+        String eventIdPrefix = dataEventRequestDTO.getDataEventType().name() + assessmentsDataDTO.getAssessments().getAssessmentId();
         Assertions.assertEquals(eventIdPrefix, payload.getEventId().substring(0, eventIdPrefix.length()));
         Assertions.assertSame(assessmentsDataDTO, payload.getPayload());
         Assertions.assertSame(dataEventRequestDTO.getEventDescription(), payload.getEventDescription());
         Assertions.assertSame(dataEventRequestDTO.getDataEventType(), payload.getEventType());
-        Assertions.assertEquals("assessments"+assessmentsDataDTO.getOrganizationId(), m.getHeaders().get(KafkaHeaders.KEY));
+        Assertions.assertEquals("assessments"+assessmentsDataDTO.getAssessments().getOrganizationId(), m.getHeaders().get(KafkaHeaders.KEY));
         return true;
       }));
   }
