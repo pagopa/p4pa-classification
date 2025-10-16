dfc383471e94aaf4ef2ffb3aefaada3869843901
michelemazzini <113985823+michelemazzini@users.noreply.github.com>
2025-10-16 14:59:41 +0200

    feat: P4ADEV-3938 add receipt id to payment reporting details (#174)

--- src/main/java/it/gov/pagopa/pu/classification/repository/PaymentsReportingRepository.java
+++ src/main/java/it/gov/pagopa/pu/classification/repository/PaymentsReportingRepository.java
@@ -1,16 +1,10 @@
 package it.gov.pagopa.pu.classification.repository;
 
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
 import it.gov.pagopa.pu.classification.model.PaymentsReporting;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.time.LocalDate;
 import java.util.List;
 
 
@@ -27,21 +21,4 @@ public interface PaymentsReportingRepository extends JpaRepository<PaymentsRepor
     "p.iur=:iur AND " +
     "p.transferIndex=:transferIndex")
   List<PaymentsReporting> findByTransferSemanticKey (Long organizationId, String iuv, String iur, int transferIndex);
-
-  @Query("""
-    SELECT p
-    FROM PaymentsReporting p
-    WHERE p.organizationId = :organizationId
-      AND p.iuf = :iuf
-      AND (:iuv IS NULL OR p.iuv = :iuv)
-      AND (cast(:payDateFrom AS DATE) IS NULL OR p.payDate >= :payDateFrom)
-      AND (cast(:payDateTo AS DATE) IS NULL OR p.payDate <= :payDateTo)
-    """)
-  Page<PaymentsReporting> findPaymentsReportingByFilters(
-    @Parameter(required = true, schema = @Schema(type = "integer", format = "int64")) @Param("organizationId") Long organizationId,
-    @Parameter(required = true) @Param("iuf") String iuf,
-    String iuv,
-    LocalDate payDateFrom,
-    LocalDate payDateTo,
-    Pageable pageable);
 }
