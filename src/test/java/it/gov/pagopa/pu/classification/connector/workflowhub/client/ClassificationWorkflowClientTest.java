ffede5a2ffc936e0e1ec067dc2e0313d7c8b3c1c
LeonardoPierangeliniNttData <leonardo.pierangelini@nttdata.com>
2025-10-29 17:44:16 +0100

    feat: P4ADEV-4030 start-classify-assessments-workflow-on-events added WorkflowHub connector (#186)

---
+++ src/test/java/it/gov/pagopa/pu/classification/connector/workflowhub/client/ClassificationWorkflowClientTest.java
@@ -0,0 +1,59 @@
+package it.gov.pagopa.pu.classification.connector.workflowhub.client;
+
+import it.gov.pagopa.pu.classification.connector.workflowhub.config.WorkflowHubApisHolder;
+import it.gov.pagopa.pu.workflow.client.generated.ClassificationApi;
+import it.gov.pagopa.pu.workflow.dto.generated.WorkflowCreatedDTO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ClassificationWorkflowClientTest {
+
+  @Mock
+  private WorkflowHubApisHolder workflowHubApisHolderMock;
+  @Mock
+  private ClassificationApi classificationApiMock;
+
+  private ClassificationWorkflowClient classificationWorkflowClient;
+
+  @BeforeEach
+  void setUp() {
+    classificationWorkflowClient = new ClassificationWorkflowClient(workflowHubApisHolderMock);
+  }
+
+  @AfterEach
+  void verifyNoMoreInteractions() {
+    Mockito.verifyNoMoreInteractions(
+      workflowHubApisHolderMock,
+      classificationApiMock
+    );
+  }
+
+  @Test
+  void whenClassifyAssessmentsThenReturnWorkflowCreatedDTO() {
+    // Given
+    String accessToken = "ACCESS_TOKEN";
+    Long organizationId = 1L;
+    String iuv = "IUV";
+    String iud = "IUD";
+    WorkflowCreatedDTO expectedResult = new WorkflowCreatedDTO();
+
+    Mockito.when(workflowHubApisHolderMock.getClassificationApi(accessToken))
+      .thenReturn(classificationApiMock);
+    Mockito.when(classificationApiMock.assessmentsClassification(organizationId, iuv, iud))
+      .thenReturn(expectedResult);
+
+    // When
+    WorkflowCreatedDTO result = classificationWorkflowClient.classifyAssessments(organizationId, iuv, iud, accessToken);
+
+    // Then
+    Assertions.assertSame(expectedResult, result);
+  }
+
+}
