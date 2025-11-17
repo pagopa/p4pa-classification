64b9a8667fa2d2ca479f922327f0609449fbb51e
michelemazzini <113985823+michelemazzini@users.noreply.github.com>
2025-11-17 17:40:27 +0100

    fix: P4ADEV-4144 prevent unexpected exception (#193)

--- src/main/java/it/gov/pagopa/pu/classification/controller/DataExportsController.java
+++ src/main/java/it/gov/pagopa/pu/classification/controller/DataExportsController.java
@@ -6,31 +6,23 @@ import it.gov.pagopa.pu.classification.dto.OffsetDateTimeIntervalFilter;
 import it.gov.pagopa.pu.classification.dto.generated.PagedClassificationView;
 import it.gov.pagopa.pu.classification.dto.generated.PagedFullClassificationView;
 import it.gov.pagopa.pu.classification.enums.ClassificationsEnum;
-import it.gov.pagopa.pu.classification.exception.custom.InvalidDateTimeIntervalException;
 import it.gov.pagopa.pu.classification.service.ClassificationService;
 import it.gov.pagopa.pu.classification.util.SecurityUtils;
 import it.gov.pagopa.pu.p4paprocessexecutions.dto.generated.LocalDateIntervalFilter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Set;
 
-import static it.gov.pagopa.pu.classification.util.Utilities.isValidIntervalBetweenLocalDate;
-
 @RestController
 public class DataExportsController implements DataExportsApi {
-  private final Integer maxMonthsInterval;
   private final ClassificationService classificationService;
 
-  public DataExportsController(@Value("${data-export.classification-view.max-months-interval}") Integer maxMonthsInterval,
-                               ClassificationService classificationService) {
-    this.maxMonthsInterval = maxMonthsInterval;
+  public DataExportsController(ClassificationService classificationService) {
     this.classificationService = classificationService;
   }
 
@@ -63,7 +55,7 @@ public class DataExportsController implements DataExportsApi {
                                                                        Set<String> debtPositionTypeOrgCodes,
                                                                        Pageable pageable) {
     String accessToken = SecurityUtils.getAccessToken();
-    LocalDateIntervalFilter lastClassificationDate = validateInterval(lastClassificationDateFrom, lastClassificationDateTo);
+    LocalDateIntervalFilter lastClassificationDate = new LocalDateIntervalFilter(lastClassificationDateFrom, lastClassificationDateTo);
     LocalDateIntervalFilter payDate = new LocalDateIntervalFilter(payDateFrom, payDateTo);
     OffsetDateTimeIntervalFilter paymentDate = new OffsetDateTimeIntervalFilter(paymentDateTimeFrom, paymentDateTimeTo);
     LocalDateIntervalFilter regulationDate = new LocalDateIntervalFilter(regulationDateFrom, regulationDateTo);
@@ -105,7 +97,7 @@ public class DataExportsController implements DataExportsApi {
                                                                                Set<String> debtPositionTypeOrgCodes,
                                                                                Pageable pageable) {
     String accessToken = SecurityUtils.getAccessToken();
-    LocalDateIntervalFilter lastClassificationDate = validateInterval(lastClassificationDateFrom, lastClassificationDateTo);
+    LocalDateIntervalFilter lastClassificationDate = new LocalDateIntervalFilter(lastClassificationDateFrom, lastClassificationDateTo);
     LocalDateIntervalFilter payDate = new LocalDateIntervalFilter(payDateFrom, payDateTo);
     OffsetDateTimeIntervalFilter paymentDate = new OffsetDateTimeIntervalFilter(paymentDateTimeFrom, paymentDateTimeTo);
     LocalDateIntervalFilter regulationDate = new LocalDateIntervalFilter(regulationDateFrom, regulationDateTo);
@@ -116,16 +108,6 @@ public class DataExportsController implements DataExportsApi {
       buildExportClassificationsFilterDTO(label, lastClassificationDate, iufs, iud, iuv, iur, payDate, paymentDate, regulationDate, billDate, regionValueDate, regulationUniqueIdentifier, accountRegistryCode, billAmountCents, remittanceInformation, pspCompanyName, pspLastName, debtPositionTypeOrgCodes);
 
     return ResponseEntity.ok(classificationService.getPagedFullClassificationView(organizationId, operatorExternalUserId, exportClassificationsFilterDTO, pageable, accessToken));
-  }
-
-  private LocalDateIntervalFilter validateInterval(LocalDate dateFrom, LocalDate dateTo) {
-    if (!isValidIntervalBetweenLocalDate(dateFrom, dateTo, ChronoUnit.MONTHS, maxMonthsInterval)) {
-      throw new InvalidDateTimeIntervalException("The date interval between %s and %s cannot exceed %d months".formatted(dateFrom, dateTo, maxMonthsInterval));
-    }
-    return LocalDateIntervalFilter.builder()
-      .from(dateFrom)
-      .to(dateTo)
-      .build();
   }
 
   @SuppressWarnings("squid:S107")
--- src/test/java/it/gov/pagopa/pu/classification/controller/DataExportsControllerTest.java
+++ src/test/java/it/gov/pagopa/pu/classification/controller/DataExportsControllerTest.java
@@ -3,8 +3,6 @@ package it.gov.pagopa.pu.classification.controller;
 import it.gov.pagopa.pu.classification.dto.ExportClassificationsFilterDTO;
 import it.gov.pagopa.pu.classification.dto.generated.PagedClassificationView;
 import it.gov.pagopa.pu.classification.dto.generated.PagedFullClassificationView;
-import it.gov.pagopa.pu.classification.enums.ClassificationsEnum;
-import it.gov.pagopa.pu.classification.exception.custom.InvalidDateTimeIntervalException;
 import it.gov.pagopa.pu.classification.service.ClassificationService;
 import it.gov.pagopa.pu.classification.util.SecurityUtilsTest;
 import it.gov.pagopa.pu.classification.util.TestUtils;
@@ -12,7 +10,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -22,12 +19,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import uk.co.jemos.podam.api.PodamFactory;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,8 +35,7 @@ class DataExportsControllerTest {
 
   @BeforeEach
   void setUp() {
-    Integer maxMonthsInterval = 1;
-    controller = new DataExportsController(maxMonthsInterval, classificationServiceMock);
+    controller = new DataExportsController(classificationServiceMock);
     SecurityUtilsTest.configureSecurityContext(accessToken, "userId");
   }
 
@@ -106,46 +97,6 @@ class DataExportsControllerTest {
   }
 
   @Test
-  void exportClassificationsThrowsInvalidDateTimeIntervalExceptionOnInvalidInterval() {
-    Pageable pageable = PageRequest.of(0, 10);
-
-    LocalDate invalidFrom = LocalDate.now();
-    LocalDate invalidTo = invalidFrom.plusMonths(3);
-    Executable executable = () ->
-      controller.exportClassifications(
-        1L,
-        "operator123",
-        Set.of(ClassificationsEnum.TES_NO_MATCH),
-        List.of("iuf_value"),
-        "iud_value",
-        List.of("iuv_value"),
-        List.of("iur_value"),
-        invalidFrom,
-        invalidTo,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        "regUniqueId",
-        "accRegistryCode",
-        1000L,
-        "remittanceInfo",
-        "pspCompany",
-        "pspLastName",
-        Set.of("code"),
-        pageable
-      );
-
-    assertThrows(InvalidDateTimeIntervalException.class, executable);
-  }
-
-  @Test
   void exportFullClassificationsReturnsPagedFullClassificationViewOnValidInterval() {
     Long organizationId = 1L;
     String operatorExternalUserId = "operator123";
@@ -193,43 +144,5 @@ class DataExportsControllerTest {
 
     assertEquals(HttpStatus.OK, response.getStatusCode());
     assertEquals(expectedView, response.getBody());
-  }
-
-  @Test
-  void exportFullClassificationsThrowsInvalidDateTimeIntervalExceptionOnInvalidInterval() {
-    LocalDate now = LocalDate.now();
-    Pageable pageable = PageRequest.of(0, 10);
-    LocalDate invalidFrom = now.minusMonths(2);
-
-    Executable executable = () -> controller.exportFullClassifications(
-        1L,
-        "operator123",
-        Set.of(ClassificationsEnum.TES_NO_MATCH),
-        List.of("iuf_value"),
-        "iud_value",
-        List.of("iuv_value"),
-        List.of("iur_value"),
-        invalidFrom,
-        now,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        "regUniqueId",
-        "accRegistryCode",
-        1000L,
-        "remittanceInfo",
-        "pspCompany",
-        "pspLastName",
-        Set.of("code"),
-        pageable);
-
-    assertThrows(InvalidDateTimeIntervalException.class, executable);
   }
 }
