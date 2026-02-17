75c14ece20b0e76638979459a985913c19bd128d
antonio.torre <antonio.torre@pagopa.it>
2026-02-17 10:46:55 +0100

    fix: Fix PersonalDataService#get2All

--- src/test/java/it/gov/pagopa/pu/common/pii/citizen/service/PersonalDataServiceTest.java
+++ src/test/java/it/gov/pagopa/pu/common/pii/citizen/service/PersonalDataServiceTest.java
@@ -30,6 +30,10 @@ import java.util.stream.Stream;
 @ExtendWith(MockitoExtension.class)
 class PersonalDataServiceTest {
 
+  public static final Class<ClassificationViewDTO> CLASS_PII_DTO = ClassificationViewDTO.class;
+  public static final PersonalDataType PERSONAL_DATA_TYPE = PersonalDataType.CLASSIFICATION;
+  public static final Class<ReceiptPIIDTO> CLASS_PII_DTO2 = ReceiptPIIDTO.class;
+
   @Mock
   private PersonalDataRepository repositoryMock;
   @Mock
@@ -63,21 +67,21 @@ class PersonalDataServiceTest {
     byte[] cipherData = new byte[0];
     Mockito.when(cipherServiceMock.encryptObj(pii)).thenReturn(cipherData);
     PersonalData personalDataInput = PersonalData.builder()
-      .type("CLASSIFICATION")
+      .type(PERSONAL_DATA_TYPE.name())
       .data(cipherData)
       .build();
 
     long piiId = -1L;
     PersonalData personalDataOutput = PersonalData.builder()
       .id(piiId)
-      .type("CLASSIFICATION")
+      .type(PERSONAL_DATA_TYPE.name())
       .data(cipherData)
       .build();
 
     Mockito.when(repositoryMock.save(personalDataInput)).thenReturn(personalDataOutput);
 
     // When
-    long insert = service.insert(pii, PersonalDataType.CLASSIFICATION);
+    long insert = service.insert(pii, PERSONAL_DATA_TYPE);
 
     // Then
     Assertions.assertEquals(piiId, insert);
@@ -88,16 +92,16 @@ class PersonalDataServiceTest {
   void givenValidPersonalDataIdWhenGetThenOk() {
     // Given
     long personalDataId = 1L;
-    ClassificationViewDTO expected = podamFactory.manufacturePojo(ClassificationViewDTO.class);
+    ClassificationViewDTO expected = podamFactory.manufacturePojo(CLASS_PII_DTO);
     Mockito.when(repositoryMock.findById(personalDataId)).thenReturn(
-      Optional.of(PersonalData.builder().id(personalDataId).data(new byte[0]).type(PersonalDataType.CLASSIFICATION.name()).build()));
-    Mockito.when(cipherServiceMock.decryptObj(new byte[0], ClassificationViewDTO.class)).thenReturn(expected);
+      Optional.of(PersonalData.builder().id(personalDataId).data(new byte[0]).type(PERSONAL_DATA_TYPE.name()).build()));
+    Mockito.when(cipherServiceMock.decryptObj(new byte[0], CLASS_PII_DTO)).thenReturn(expected);
 
     // When
-    ClassificationViewDTO classificationViewDTO = service.get(personalDataId, ClassificationViewDTO.class);
+    ClassificationViewDTO result = service.get(personalDataId, CLASS_PII_DTO);
 
     //Then
-    Assertions.assertTrue(EqualsBuilder.reflectionEquals(expected, classificationViewDTO, true, null, true));
+    Assertions.assertTrue(EqualsBuilder.reflectionEquals(expected, result, true, null, true));
   }
 
   @Test
@@ -107,7 +111,7 @@ class PersonalDataServiceTest {
     Mockito.when(repositoryMock.findById(personalDataId)).thenReturn(Optional.empty());
 
     // When
-    NotFoundException notFoundException = Assertions.assertThrows(NotFoundException.class, () -> service.get(personalDataId, ClassificationViewDTO.class));
+    NotFoundException notFoundException = Assertions.assertThrows(NotFoundException.class, () -> service.get(personalDataId, CLASS_PII_DTO));
 
     // Then
     Assertions.assertEquals("[PII_ENTITY_NOT_FOUND] PII Entity with id 1 not found", notFoundException.getMessage());
@@ -121,19 +125,19 @@ class PersonalDataServiceTest {
     long pId1 = 1L;
     long pId2 = 2L;
     Set<Long> personalDataIds = Set.of(pId1, pId2);
-    ClassificationViewDTO pii1 = podamFactory.manufacturePojo(ClassificationViewDTO.class);
-    ClassificationViewDTO pii2 = podamFactory.manufacturePojo(ClassificationViewDTO.class);
+    ClassificationViewDTO pii1 = podamFactory.manufacturePojo(CLASS_PII_DTO);
+    ClassificationViewDTO pii2 = podamFactory.manufacturePojo(CLASS_PII_DTO);
 
     Mockito.when(repositoryMock.findAllById(personalDataIds)).thenReturn(List.of(
-      PersonalData.builder().id(pId1).data(new byte[0]).type(PersonalDataType.CLASSIFICATION.name()).build(),
-      PersonalData.builder().id(pId2).data(new byte[0]).type(PersonalDataType.CLASSIFICATION.name()).build()
+      PersonalData.builder().id(pId1).data(new byte[0]).type(PERSONAL_DATA_TYPE.name()).build(),
+      PersonalData.builder().id(pId2).data(new byte[0]).type(PERSONAL_DATA_TYPE.name()).build()
     ));
-    Mockito.when(cipherServiceMock.decryptObj(new byte[0], ClassificationViewDTO.class))
+    Mockito.when(cipherServiceMock.decryptObj(new byte[0], CLASS_PII_DTO))
       .thenReturn(pii1)
       .thenReturn(pii2);
 
     // When
-    Map<Long, ClassificationViewDTO> results = service.getAll(personalDataIds, ClassificationViewDTO.class);
+    Map<Long, ClassificationViewDTO> results = service.getAll(personalDataIds, CLASS_PII_DTO);
 
     //Then
     Assertions.assertTrue(EqualsBuilder.reflectionEquals(pii1, results.get(pId1), true, null, true));
@@ -145,14 +149,52 @@ class PersonalDataServiceTest {
     // Given
     Set<Long> personalDataIds = Set.of(1L, 2L);
     Mockito.when(repositoryMock.findAllById(personalDataIds)).thenReturn(List.of(
-      PersonalData.builder().id(1L).data(new byte[0]).type(PersonalDataType.CLASSIFICATION.name()).build()));
-    Mockito.when(cipherServiceMock.decryptObj(new byte[0], ClassificationViewDTO.class)).thenReturn(podamFactory.manufacturePojo(ClassificationViewDTO.class));
+      PersonalData.builder().id(1L).data(new byte[0]).type(PERSONAL_DATA_TYPE.name()).build()));
+    Mockito.when(cipherServiceMock.decryptObj(new byte[0], CLASS_PII_DTO)).thenReturn(podamFactory.manufacturePojo(CLASS_PII_DTO));
 
     // When
-    NotFoundException notFoundException = Assertions.assertThrows(NotFoundException.class, () -> service.getAll(personalDataIds, ClassificationViewDTO.class));
+    NotFoundException notFoundException = Assertions.assertThrows(NotFoundException.class, () -> service.getAll(personalDataIds, CLASS_PII_DTO));
 
     // Then
     Assertions.assertEquals("[PII_ENTITY_NOT_FOUND] PII Entities with ids 2 not found", notFoundException.getMessage());
+  }
+
+  @Test
+  void whenGetAllPrivateThenOk() {
+    // Given
+    long pId1 = 1L;
+    long pId2 = 2L;
+    Set<Long> searchedPDataIds = Set.of(pId1, pId2);
+    List<PersonalData> pDatas = List.of(
+      PersonalData.builder()
+        .id(pId1)
+        .data(new byte[0])
+        .type(PERSONAL_DATA_TYPE.name())
+        .build(),
+      PersonalData.builder()
+        .id(pId2)
+        .data(new byte[0])
+        .type(PERSONAL_DATA_TYPE.name())
+        .build(),
+      PersonalData.builder()
+        .id(3L)
+        .data(new byte[0])
+        .type(PERSONAL_DATA_TYPE.name())
+        .build()
+    );
+
+    ClassificationViewDTO pii1 = podamFactory.manufacturePojo(CLASS_PII_DTO);
+    ClassificationViewDTO pii2 = podamFactory.manufacturePojo(CLASS_PII_DTO);
+    Mockito.when(cipherServiceMock.decryptObj(new byte[0], CLASS_PII_DTO))
+      .thenReturn(pii1)
+      .thenReturn(pii2);
+
+    // When
+    Map<Long, ClassificationViewDTO> results = service.getAll(pDatas, searchedPDataIds, CLASS_PII_DTO);
+
+    // Then
+    Assertions.assertTrue(EqualsBuilder.reflectionEquals(pii1, results.get(pId1), true, null, true));
+    Assertions.assertTrue(EqualsBuilder.reflectionEquals(pii2, results.get(pId2), true, null, true));
   }
 //endregion
 
@@ -160,11 +202,11 @@ class PersonalDataServiceTest {
   void whenGet2AllThenOk() {
     // Given
     Set<Long> pDataIds1 = LongStream.range(0, 10).boxed().collect(Collectors.toSet());
-    Class<ClassificationViewDTO> classType1 = ClassificationViewDTO.class;
+    Class<ClassificationViewDTO> classType1 = CLASS_PII_DTO;
     Map<Long, ClassificationViewDTO> expectedPData1Dtos = Map.of();
 
     Set<Long> pDataIds2 = LongStream.range(pDataIds1.size(), 10).boxed().collect(Collectors.toSet());
-    Class<ReceiptPIIDTO> classType2 = ReceiptPIIDTO.class;
+    Class<ReceiptPIIDTO> classType2 = CLASS_PII_DTO2;
     Map<Long, ReceiptPIIDTO> expectedPData2Dtos = Map.of();
 
     service = Mockito.spy(service);
