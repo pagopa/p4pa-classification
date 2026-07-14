b4f389231e0a6181f5b4b7bb543994ee22fae28e
antocalo <antonio.calo@nttdata.com>
2026-07-14 11:38:42 +0200

    P4ADEV-4834-validate-string-balance

--- src/test/java/it/gov/pagopa/pu/classification/service/BalanceServiceTest.java
+++ src/test/java/it/gov/pagopa/pu/classification/service/BalanceServiceTest.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
