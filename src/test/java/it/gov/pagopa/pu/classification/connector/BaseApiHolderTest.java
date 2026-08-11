909a669b85d5175273a4fb3b8d801a93653f2f20
antonio.torre <antonio.torre@pagopa.it>
2026-08-11 05:28:40 +0200

    fix: Returning field error details

--- src/test/java/it/gov/pagopa/pu/classification/connector/BaseApiHolderTest.java
+++ src/test/java/it/gov/pagopa/pu/classification/connector/BaseApiHolderTest.java
@@ -64,7 +64,8 @@ public abstract class BaseApiHolderTest {
 
     Assertions.assertEquals(
       apiPackage
-        .replaceFirst("^.+\\.", ""),
+        .replaceFirst("it\\.gov\\.pagopa\\.(pu\\.)?", "")
+        .replace(".", ""),
       errorHandler.getApplicationName()
         .toLowerCase()
         .replace("_","")
